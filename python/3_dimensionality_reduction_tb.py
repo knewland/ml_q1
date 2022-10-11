3c57ee2c1c908d5d690e0182178074421f03e84b
knewland <67282502+knewland@users.noreply.github.com>
2022-10-11 00:19:51 -0400

    Update 3_dimensionality_reduction_tb.py

--- python/3_dimensionality_reduction_tb.py
+++ python/3_dimensionality_reduction_tb.py
@@ -13,18 +13,17 @@ with open('data_x.npy','rb') as f:
     data_x = np.load(f)
 with open('data_y.npy','rb') as f:
     data_y = np.load(f)
-print(data_x.shape)
-print(data_y.shape)
 
 clf = ExtraTreesClassifier(n_estimators=50)
 clf = clf.fit(data_x, data_y)
 
 print("feature importances:")
 print(clf.feature_importances_)
-
+print()
 model = SelectFromModel(clf, prefit=True)
 data_x_new = model.transform(data_x)
-print(data_x_new.shape)
+print("Original number of attributes:", data_x.shape[1])
+print("New number of attributes (TB):", data_x_new.shape[1])
 #saving numpy array for next step
 with open('tb_data_x.npy','wb') as f:
     np.save(f, data_x_new)
